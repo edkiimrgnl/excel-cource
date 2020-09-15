e1596c737c5ce999c78375d4a1efea00de682737
Егорк Орнилов <edkiimrgnl@gmail.com>
2020-09-15 20:30:19 +0300

    finish base framework functional

---
+++ src/components/excel/Excel.js
@@ -0,0 +1,36 @@
+import {$} from '@core/dom'
+
+export class Excel {
+  constructor(selector, options) {
+    this.$el = $(selector)
+    this.components = options.components || []
+  }
+
+  getRoot() {
+    const $root = $.create('div', 'excel')
+
+    this.components = this.components.map(Component => {
+      // const $el = document.createElement('div')
+      // $el.classList.add(Component.className)
+      const $el = $.create('div', Component.className)
+      const component = new Component($el)
+      // DEBUG
+      if (component.name) {
+        window['c' + component.name] = component
+      }
+      $el.html(component.toHTML())
+      $root.append($el)
+      return component
+    })
+
+    return $root
+  }
+
+  render() {
+    // afterbegin, afterend, beforebegin, beforeend
+    // this.$el.insertAdjacentHTML('afterbegin', `h1`)
+    this.$el.append(this.getRoot())
+
+    this.components.forEach(component => component.init())
+  }
+}
