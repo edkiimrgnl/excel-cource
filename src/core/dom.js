9ed93fb1c5d04d762a8056feba5779e0bf53afc6
Егорк Орнилов <edkiimrgnl@gmail.com>
2020-10-02 20:37:10 +0300

    table selection part 1 - finish base table logic

--- src/core/dom.js
+++ src/core/dom.js
@@ -14,6 +14,17 @@ class Dom {
     return this.$el.outerHTML.trim()
   }
 
+  text(text) {
+    if (typeof text === 'string') {
+      this.$el.textContent = text
+      return this
+    }
+    if (this.$el.tagName.toLowerCase() === 'input') {
+      return this.$el.value.trim()
+    }
+    return this.$el.textContent.trim()
+  }
+
   clear() {
     this.html('')
     return this
@@ -79,8 +90,14 @@ class Dom {
     return this.data.id
   }
 
+  focus() {
+    this.$el.focus()
+    return this
+  }
+
   addClass(className) {
     this.$el.classList.add(className)
+    return this
   }
 
   removeClass(className) {
